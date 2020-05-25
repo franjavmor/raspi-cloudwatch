773f386fbbd2f09c50bbbdce08a0e021daa6697d
Francisco Javier Morales <fmorales@ar.ibm.com>
2020-05-25 19:25:08 -0300

    Update de README.md, primera versión de raspi-dht-cloudwatch.py y requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# raspi-cloudwatch
+# Raspberry Pi + DHT + CloudWatch
 Usando Raspberry Pi con AWS CloudWatch
+
+## Ambiente de prueba
+ - Raspberry Pi 3 B+
+ - Raspbian 4.19
+ - Sensor DHT22
+ - AWS CloudWatch
+
+## Idea
+Adaptación de la guía de Bart Castle llamada *Raspberry Pi monitoring with AWS CloudWatch Dashboard*
+
+Fuente: [Raspberry Pi monitoring with AWS CloudWatch Dashboard](https://www.youtube.com/watch?v=kSZzAairBHo)
+
+## Requerimientos
+    pip3 install -r requirements.txt
---
+++ raspi-dht-cloudwatch.py
@@ -0,0 +1,47 @@
+import boto3
+import board
+import adafruit_dht
+
+# Especifico el PIN 15 (GPIO22)
+dhtDevice = adafruit_dht.DHT22(board.D22)
+
+# Obtengo los valores de temperatura y humedad
+temperature = dhtDevice.temperature
+humidity = dhtDevice.humidity
+
+temperature = round(temperature,2)
+humidity = round(humidity,2)
+
+print ('Temperature: ',temperature,', Humidity: ',humidity)
+
+# AWS credentials and region needs to be added
+# Credentials: ~/.aws/credentials
+# Region: ~/.aws/config
+# Guide: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html#guide-configuration
+
+# CloudWatch
+cloudwatch = boto3.client('cloudwatch')
+
+# Add temperature metric
+cloudwatch.put_metric_data(
+    Namespace='raspifran',
+    MetricData=[
+        {
+            'MetricName': 'Temperature',
+            'Value': temperature,
+            'Unit': 'None'
+        },
+    ]
+)
+
+# Add humidity metric
+cloudwatch.put_metric_data(
+    Namespace='raspifran',
+    MetricData=[
+        {
+            'MetricName': 'Humidity',
+            'Value': humidity,
+            'Unit': 'None'
+        },
+    ]
+)
